e1961928e7b290f9d1086e311cd2167a489762c6
Derrick Cullen <derrick.cullen1@gmail.com>
2015-11-08 17:31:52 -0500

    added secondUbuntu.cpp

---
+++ secondUbuntu.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	cout << "Hello World \n This is my second C++ project don on UBUNTU" << endl;
+
+	int user_input;
+
+	cin >> user_input;
+
+	return 0;
+}
